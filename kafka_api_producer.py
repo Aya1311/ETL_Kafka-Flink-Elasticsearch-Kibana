8445cb8033756d43d0e91d187d3d66fad60fbc52
AyaLaadaili <118177059+Aya1311@users.noreply.github.com>
2025-01-06 11:26:09 +0100

    Update kafka_api_producer.py

--- kafka_api_producer.py
+++ kafka_api_producer.py
@@ -11,7 +11,7 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda K:dumps(K).encode('utf-8'))
 # Parameters to get the first 10 repositories
 # Init
-newsapi = NewsApiClient(api_key='49c02fcde33f41a4b5c093011c4e7b91')
+newsapi = NewsApiClient(api_key='.......')
 
 # /v2/top-headlines
 top_headlines = newsapi.get_top_headlines()
